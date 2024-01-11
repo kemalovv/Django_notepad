0d21ac6cb27c0460d851bd6bb52e818af6c34149
Kemalov Daniyar <kemalov2011@mail.ru>
2024-01-11 16:36:12 +0300

    Create a model Entry

--- django_notepad/notepad/models.py
+++ django_notepad/notepad/models.py
@@ -9,3 +9,19 @@ class Topic(models.Model):
     def __str__(self):
         """Returning str representation of a model"""
         return self.text
+
+
+class Entry(models.Model):
+    """Information that user has studied"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        """Returning str representation of a model"""
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        return self.text
