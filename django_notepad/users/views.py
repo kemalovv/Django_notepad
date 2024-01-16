a1d4c2045921430d77c29a0e1507a94356a8cce4
Kemalov Daniyar <kemalov2011@mail.ru>
2024-01-16 16:31:32 +0300

    Now users can registrate by themselves

--- django_notepad/users/views.py
+++ django_notepad/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    """"Register a new user"""
+    if request.method != "POST":
+        # Empty registration form
+        form = UserCreationForm()
+    else:
+        # Processing of the completed form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Performing an entry and redirect to homepage
+            login(request, new_user)
+            return redirect('notepad:index')
+
+    # Shows empty or incorrect form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
+
